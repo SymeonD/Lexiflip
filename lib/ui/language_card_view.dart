63802abd87e9e2953567c8579059f9cccfa54f69
De Almeida Syméon <symeon.dealmeida@gmail.com>
2025-02-22 13:22:08 +1000

    Change text field in cards list

--- lib/ui/language_card_view.dart
+++ lib/ui/language_card_view.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:cards/models/database_helper.dart';
 import 'package:cards/models/language.dart';
 // import 'package:cards/models/language.dart';
@@ -57,156 +58,159 @@ class _LanguageCardViewState extends State<LanguageCardView> {
               borderRadius: BorderRadius.circular(12.0),
             ),
             color: Colors.white,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
+            child: Stack(
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    PopupMenuButton<String>(
-                      onSelected: (value) {
-                        // Handle menu selection
-                        if (value == "edit") {
-                          // TODO: See if kept here ?
-                          // Open the page to edit the card
-                          Navigator.push(context,
-                              MaterialPageRoute(builder: (context) {
-                            return CardDialogView(
-                              language: widget.language,
-                              languageCard: widget.languageCard,
-                              onCardAdded: widget.onDelete,
-                              languageDeck: widget.languageDeck,
-                              cardTag:
-                                  widget.languageCard.languageCardId.toString(),
-                            );
-                          }));
-                        } else if (value == "share") {
-                          // Share the deck
-                          // Show a bar at the bottom of the screen with a text 'Coming soon'
-                          showCustomSnackBar(context, "Coming soon", 2);
-                        } else if (value == "remove") {
-                          // Remove the card from the deck
-                          DatabaseHelper.instance.removeCardFromDeck(
-                            widget.languageDeck.languageDeckId!,
-                            widget.languageCard.languageCardId!,
+                Positioned(
+                  right: 0,
+                  child: PopupMenuButton<String>(
+                    onSelected: (value) {
+                      // Handle menu selection
+                      if (value == "edit") {
+                        // TODO: See if kept here ?
+                        // Open the page to edit the card
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                          return CardDialogView(
+                            language: widget.language,
+                            languageCard: widget.languageCard,
+                            onCardAdded: widget.onDelete,
+                            languageDeck: widget.languageDeck,
+                            cardTag:
+                                widget.languageCard.languageCardId.toString(),
                           );
-                          widget.onDelete();
-                        } else if (value == "delete") {
-                          showDialog(
-                              context: context,
-                              builder: (context) {
-                                return AlertDialog(
-                                  title: const Text("Delete card"),
-                                  content: const Text(
-                                      "Are you sure you want to delete this card?"),
-                                  actions: [
-                                    TextButton(
-                                      child: const Text("Cancel"),
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                      },
-                                    ),
-                                    TextButton(
-                                      child: const Text("Delete"),
-                                      onPressed: () {
-                                        DatabaseHelper.instance.deleteCard(
-                                          widget.languageCard.languageCardId!,
-                                        );
-                                        widget.onDelete();
-                                        // Delete the card
-                                        Navigator.of(context).pop();
-                                      },
-                                    ),
-                                  ],
-                                );
-                              });
-                        }
-                      },
-                      offset: const Offset(0, 40),
-                      color: Colors.white,
-                      icon: const Icon(Icons.more_horiz),
-                      itemBuilder: (BuildContext context) => [
-                        // Edit option
-                        const PopupMenuItem(
-                          value: "edit",
-                          height: 35,
-                          padding: EdgeInsets.only(left: 10),
-                          child: IntrinsicWidth(
-                            child: SizedBox(
-                              width: 100,
-                              child: Row(
-                                children: [
-                                  Icon(Icons.edit_outlined,
-                                      color: Colors.black),
-                                  SizedBox(width: 10),
-                                  Text("Edit"),
+                        }));
+                      } else if (value == "share") {
+                        // Share the deck
+                        // Show a bar at the bottom of the screen with a text 'Coming soon'
+                        showCustomSnackBar(context, "Coming soon", 2);
+                      } else if (value == "remove") {
+                        // Remove the card from the deck
+                        DatabaseHelper.instance.removeCardFromDeck(
+                          widget.languageDeck.languageDeckId!,
+                          widget.languageCard.languageCardId!,
+                        );
+                        widget.onDelete();
+                      } else if (value == "delete") {
+                        showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: const Text("Delete card"),
+                                content: const Text(
+                                    "Are you sure you want to delete this card?"),
+                                actions: [
+                                  TextButton(
+                                    child: const Text("Cancel"),
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                  ),
+                                  TextButton(
+                                    child: const Text("Delete"),
+                                    onPressed: () {
+                                      DatabaseHelper.instance.deleteCard(
+                                        widget.languageCard.languageCardId!,
+                                      );
+                                      widget.onDelete();
+                                      // Delete the card
+                                      Navigator.of(context).pop();
+                                    },
+                                  ),
                                 ],
-                              ),
-                            ),
-                          ),
-                        ),
-                        // Share option
-                        const PopupMenuItem(
-                          value: "share",
-                          height: 35,
-                          child: IntrinsicWidth(
-                            child: SizedBox(
-                              width: 100,
-                              child: Row(
-                                children: [
-                                  Icon(Icons.share_outlined,
-                                      color: Colors.black),
-                                  SizedBox(width: 10),
-                                  Text("Share"),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                        const PopupMenuDivider(),
-                        // If isDefault is false, remove from deck option
-                        if (!widget.languageDeck.isDefault!)
-                          const PopupMenuItem(
-                            value: "remove",
-                            height: 35,
-                            child: SizedBox(
-                              child: Row(
-                                children: [
-                                  Icon(Icons.remove_circle_outline,
-                                      color: Colors.red),
-                                  SizedBox(width: 10),
-                                  Text(
-                                    "Remove from deck",
-                                  )
-                                ],
-                              ),
-                            ),
-                          ),
-
-                        // Delete option
-                        const PopupMenuItem(
-                          value: "delete",
-                          height: 35,
+                              );
+                            });
+                      }
+                    },
+                    offset: const Offset(0, 40),
+                    color: Colors.white,
+                    icon: const Icon(Icons.more_horiz),
+                    itemBuilder: (BuildContext context) => [
+                      // Edit option
+                      const PopupMenuItem(
+                        value: "edit",
+                        height: 35,
+                        padding: EdgeInsets.only(left: 10),
+                        child: IntrinsicWidth(
                           child: SizedBox(
                             width: 100,
                             child: Row(
                               children: [
-                                Icon(Icons.delete_outlined, color: Colors.red),
+                                Icon(Icons.edit_outlined, color: Colors.black),
                                 SizedBox(width: 10),
-                                Text("Delete",
-                                    style: TextStyle(color: Colors.red)),
+                                Text("Edit"),
                               ],
                             ),
                           ),
                         ),
-                      ],
-                    ),
-                  ],
+                      ),
+                      // Share option
+                      const PopupMenuItem(
+                        value: "share",
+                        height: 35,
+                        child: IntrinsicWidth(
+                          child: SizedBox(
+                            width: 100,
+                            child: Row(
+                              children: [
+                                Icon(Icons.share_outlined, color: Colors.black),
+                                SizedBox(width: 10),
+                                Text("Share"),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      const PopupMenuDivider(),
+                      // If isDefault is false, remove from deck option
+                      if (!widget.languageDeck.isDefault!)
+                        const PopupMenuItem(
+                          value: "remove",
+                          height: 35,
+                          child: SizedBox(
+                            child: Row(
+                              children: [
+                                Icon(Icons.remove_circle_outline,
+                                    color: Colors.red),
+                                SizedBox(width: 10),
+                                Text(
+                                  "Remove from deck",
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+
+                      // Delete option
+                      const PopupMenuItem(
+                        value: "delete",
+                        height: 35,
+                        child: SizedBox(
+                          width: 100,
+                          child: Row(
+                            children: [
+                              Icon(Icons.delete_outlined, color: Colors.red),
+                              SizedBox(width: 10),
+                              Text("Delete",
+                                  style: TextStyle(color: Colors.red)),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ), // Add space between the row and text
-                Text(widget.languageCard.nativeText,
-                    style: const TextStyle(
-                        fontSize: 18, fontWeight: FontWeight.bold)),
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 16, left: 16),
+                    child: AutoSizeText(widget.languageCard.nativeText,
+                        maxLines: 2,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        )),
+                  ),
+                ),
               ],
             ),
           ),
